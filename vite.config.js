88d9b0d7d5120409de634d8c33a0e4e2f3caf5d7
潘丞詡 <billy890917@gmail.com>
2024-10-02 21:59:53 +0800

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MUN\Society\Taiwan/",
+  base: "/MUN-Society-Taiwan/",
 })
