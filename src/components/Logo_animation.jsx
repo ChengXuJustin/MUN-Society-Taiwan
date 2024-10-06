8271c1b31c33f2f6ecdd2c320be6d23369bb2be0
潘丞詡 <billy890917@gmail.com>
2024-10-06 08:03:39 +0800

    add: explore button cellphone version

--- src/components/Logo_animation.jsx
+++ src/components/Logo_animation.jsx
@@ -5,7 +5,7 @@ import '../css/explore_btn.css'
 
 const Logo_animation = () => {
   return (
-    <div style={{marginTop:"-150px"}}>
+    <div className="Logo_Animation" style={{marginTop:"-150px"}}>
           <div className="Logo_Container">
             <video className="Logo_video" src="MST_Logo_animation.mp4" autoPlay muted playsInline>
               {/* <source src={mstIcon.logo_gif} type='video/mp4' /> */}
@@ -13,7 +13,7 @@ const Logo_animation = () => {
             </video>
             <div className="hidenBox"></div>
           </div>
-          <div>
+          <div style={{position:"absolute"}}>
             <Link to="MUN-Society-Taiwan/about" className="Explore">
               <li className='innerTxt'>Explore MST</li>
             </Link>
